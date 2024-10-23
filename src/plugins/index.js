c82a4a70247b0acfaea8ab850137df2db9594b7f
chenqingsong <821570533@qq.com>
2024-10-23 16:50:57 +0800

    feat:初始化项目

--- src/plugins/index.js
+++
@@ -1,7 +0,0 @@
-import { loadElementPlus } from "./element-plus"
-import { loadElementPlusIcon } from "./element-plus-icon"
-
-export function loadPlugins(app) {
-  loadElementPlus(app)
-  loadElementPlusIcon(app)
-}
